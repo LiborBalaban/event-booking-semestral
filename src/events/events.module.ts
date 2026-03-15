2ef701a372064d9efa89c2519214b0755034a306
Libor <libor.balaban10@seznam.cz>
2026-03-15 15:33:14 +0100

    feat: implementace logiky pro hlídání kapacity události (GREEN)

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [EventsService]
+  providers: [EventsService, PrismaService]
 })
 export class EventsModule {}
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
-export class EventsService {}
+export class EventsService {
+  constructor(private prisma: PrismaService) {}
+
+  async registerUserForEvent(userId: string, eventId: string) {
+    const event = await this.prisma.event.findUnique({
+      where: { id: eventId },
+      include: { registrations: true },
+    });
+
+    if (!event) {
+      throw new NotFoundException('Událost nebyla nalezena');
+    }
+
+    if (event.registrations.length >= event.capacity) {
+      throw new BadRequestException('Událost je již vyprodaná');
+    }
+
+    return this.prisma.registration.create({
+      data: {
+        userId,
+        eventId,
+      },
+    });
+  }
+}
