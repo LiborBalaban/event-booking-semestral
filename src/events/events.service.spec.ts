6f8dbf6d3d2aff12892a237a087f0721a230dfa2
Libor <libor.balaban10@seznam.cz>
2026-03-15 15:38:34 +0100

    test: přidání testu na zamezení duplicitních přihlášek (RED)

--- src/events/events.service.spec.ts
+++ src/events/events.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { EventsService } from './events.service';
 import { PrismaService } from '../prisma.service';
 import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, ConflictException } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 describe('EventsService', () => {
@@ -42,4 +42,47 @@ describe('EventsService', () => {
       await expect(service.registerUserForEvent(userId, eventId)).rejects.toThrow(BadRequestException);
     });
   });
+
+  describe('Pravidlo 1: Omezení kapacity', () => {
+    it('měl by vyhodit BadRequestException, pokud je událost vyprodaná', async () => {
+      const eventId = 'event-1';
+      const userId = 'user-1';
+
+      prismaMock.event.findUnique.mockResolvedValue({
+        id: eventId,
+        name: 'Super koncert',
+        capacity: 2,
+        date: new Date(),
+        isAdultOnly: false,
+        registrations: [
+          { id: 'reg1', userId: 'u1', eventId, createdAt: new Date() },
+          { id: 'reg2', userId: 'u2', eventId, createdAt: new Date() }
+        ]
+      } as any);
+
+      await expect(service.registerUserForEvent(userId, eventId)).rejects.toThrow(BadRequestException);
+    });
+  });
+
+  describe('Pravidlo 2: Zabránění duplicitě', () => {
+    it('měl by vyhodit ConflictException, pokud se uživatel hlásí na stejnou událost podruhé', async () => {
+
+      const eventId = 'event-1';
+      const userId = 'user-1';
+
+      prismaMock.event.findUnique.mockResolvedValue({
+        id: eventId,
+        name: 'Super koncert',
+        capacity: 10,
+        date: new Date(),
+        isAdultOnly: false,
+        registrations: [
+          { id: 'reg1', userId: userId, eventId: eventId, createdAt: new Date() }
+        ]
+      } as any);
+
+      await expect(service.registerUserForEvent(userId, eventId)).rejects.toThrow(ConflictException);
+    });
+  });
+
 });
