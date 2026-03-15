09218605a943a25d36e90c80e0710405042ddd68
Libor <libor.balaban10@seznam.cz>
2026-03-15 20:46:27 +0100

    fix: oprava vygenerovaného testu pro EventsController

--- src/events/events.controller.spec.ts
+++ src/events/events.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventsController } from './events.controller';
+import { EventsService } from './events.service';
+import { PrismaService } from '../prisma.service';
 
 describe('EventsController', () => {
   let controller: EventsController;
@@ -7,6 +9,16 @@ describe('EventsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EventsController],
+      providers: [
+        {
+          provide: EventsService,
+          useValue: {},
+        },
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     controller = module.get<EventsController>(EventsController);
